e92a3ef8b9c4688918a41d58d9cd29e76c695f36
ayerxd <ayrtonu99@gmail.com>
2025-03-16 14:05:15 -0500

    mejoras

--- src/main/java/pe/edu/idat/demo_chat_websocket/Config/WebSocketConfig.java
+++ src/main/java/pe/edu/idat/demo_chat_websocket/Config/WebSocketConfig.java
@@ -1,36 +1,27 @@
 package pe.edu.idat.demo_chat_websocket.Config;
 
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private static final Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
-
-        @Override
-        public void registerStompEndpoints(StompEndpointRegistry registry) {
-            registry.addEndpoint("/ws")
-                    .setAllowedOrigins("*")
-                    .withSockJS();
-
-
-        logger.info("STOMP endpoint '/ws' registrado");
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/notificaciones").withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-        logger.info("Broker configurado con '/topic' y prefijos '/app'");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
     }
 }
+
 
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,1 +1,31 @@
+let stompClient = null;
 
+function conectar() {
+    let socket = new SockJS('/notificaciones');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Conectado: ' + frame);
+        stompClient.subscribe('/topic/notificaciones', function (mensaje) {
+            mostrarMensaje(JSON.parse(mensaje.body));
+        });
+    });
+}
+
+function enviarMensaje() {
+    let emisor = document.getElementById("emisor").value;
+    let mensaje = document.getElementById("mensaje").value;
+    if (stompClient && emisor && mensaje) {
+        let msgObj = { emisor: emisor, contenido: mensaje };
+        stompClient.send("/app/enviar", {}, JSON.stringify(msgObj));
+        document.getElementById("mensaje").value = "";
+    }
+}
+
+function mostrarMensaje(msg) {
+    let notificacionesDiv = document.getElementById("notificaciones");
+    let p = document.createElement("p");
+    p.innerText = msg.emisor + ": " + msg.contenido;
+    notificacionesDiv.appendChild(p);
+}
+
+window.
